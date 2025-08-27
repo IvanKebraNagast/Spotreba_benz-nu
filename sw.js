a4c42a997344f774118b66964728376d78222706
IvanKebraNagast <konopkaivan@gmail.com>
2025-08-27 13:20:10 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,18 @@
+const CACHE = "cesta-v1";
+const FILES = [
+  "index.html",
+  "manifest.webmanifest",
+  "icon.png"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(FILES))
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
